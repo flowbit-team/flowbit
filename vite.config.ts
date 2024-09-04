5f2c4d50bb26b683991008bc5b1f070a24b0f27b
klmhyeonwoo <gentlemonster77@likelion.org>
2024-09-04 20:36:43 +0900

    Add Google Analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { VitePluginRadar } from "vite-plugin-radar";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +17,11 @@ export default defineConfig({
       },
     }),
     svgr(),
+    VitePluginRadar({
+      analytics: {
+        id: process.env.VITE_GOOGLE_ANALYTICS,
+      },
+    }),
   ],
   resolve: {
     alias: {
