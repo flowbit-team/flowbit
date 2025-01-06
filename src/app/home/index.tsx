c45b812cace962da329877120f4146a2fadebce8
송조은 <goods@jiran.com>
2025-01-06 21:27:37 +0900

    feat: 조회수 페이지 UI

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -20,14 +20,18 @@ import Chart3 from "@/assets/chart3.svg";
 import MAINBOTTOMIMG from "@/assets/main-bottom.gif";
 import NewletterAni from "@/assets/newletter.json";
 import CommunityAni from "@/assets/community.json";
+import BlackEtherImg from "@/assets/black_ether.svg";
+import SkyblueLImg from "@/assets/skyblue_L.svg";
+import GreenBitImg from "@/assets/green_bit.svg";
+import YellowBitImg from "@/assets/yellow_bit.svg";
+import OrangeBitImg from "@/assets/orange_bit.svg";
 import { useGetChartDataQuery } from "@/api/chartApi";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useModal } from "@/hooks/useModal.ts";
+import { SubscriptionModalContent } from "@/components/common/modal/SubscriptionModalContent";
 import { useAtom } from "jotai";
 import { loginState } from "@/store/user";
-import { SubscriptionModalContent } from "@/components/common/modal/SubscriptionModalContent";
-
 
 type CoinInfoType = {
   [coin in "BTC" | "ETH" | "XRP"]: {
@@ -112,14 +116,14 @@ export default function HomePage() {
     }
   };
 
-   /**
+  /**
    * @description 구독버튼 클릭시 flowbit서비스에 대해 주기적으로 구독할 수 있는 함수입니다.
    */
-   const openSubscriptionModal = () => {
+  const openSubscriptionModal = () => {
     open({
       title: "구독 설정",
       content: <SubscriptionModalContent />,
-      isVisibleBtn: false
+      isVisibleBtn: false,
     });
   };
 
@@ -161,10 +165,57 @@ export default function HomePage() {
         >
           {/* LEFT SIDE */}
           <div>
+            <div
+              css={css`
+                position: relative;
+                width: 31.5rem;
+                height: 4rem;
+                padding: 0px;
+                background: #2f3b4b;
+                color: #ffffff;
+                border-radius: 1rem;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                &::after {
+                  content: "";
+                  position: absolute;
+                  width: 22px;
+                  height: 20px;
+                  background: #2f3b4b;
+                  clip-path: path("M0,0 L22,0 L13,16 Q11,20 9,16 L0,0");
+                  display: block;
+                  z-index: 1;
+                  margin-left: -2.6rem;
+                  bottom: -1rem;
+                  left: 13%;
+                }
+
+                ${BREAK_POINTS.TABLET} {
+                }
+
+                ${BREAK_POINTS.MOBILE} {
+                }
+              `}
+            >
+              <p>
+                <span
+                  css={css`
+                    color: #33c2ff;
+                  `}
+                >
+                  {/* {totalVisit} */}
+                  10,404,174
+                </span>
+                명이 플로우빗과 함께하고 있어요
+              </p>
+            </div>
             <h1
               css={css`
                 font-size: 4.8rem;
                 line-height: 7.2rem;
+                margin-top: 20px;
 
                 ${BREAK_POINTS.TABLET} {
                 }
@@ -526,6 +577,151 @@ export default function HomePage() {
           src={MAINBOTTOMIMG}
           alt=""
         />
+      </section>
+      {/* 하단 배너 */}
+      <section>
+        <div
+          css={css`
+            padding: 12rem 0;
+
+            ${BREAK_POINTS.TABLET} {
+              padding: 8rem 0;
+            }
+
+            ${BREAK_POINTS.MOBILE} {
+              padding: 6rem 0;
+            }
+          `}
+        >
+          <div
+            css={css`
+              position: relative;
+              width: 100%;
+              height: 16.4rem;
+              border-radius: 1.6rem;
+              background-color: #0044a1;
+              font-weight: 600;
+              font-size: 2.8rem;
+              line-height: 4.2rem;
+              color: #ffffff;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              text-align: center;
+              overflow: hidden;
+
+              ${BREAK_POINTS.TABLET} {
+                height: 14rem;
+                font-size: 2.4rem;
+                line-height: 3.6rem;
+              }
+
+              ${BREAK_POINTS.MOBILE} {
+                height: 7.2rem;
+                border-radius: 1rem;
+                font-size: 1.2rem;
+                line-height: 1.8rem;
+              }
+            `}
+          >
+            <p>
+              <span
+                css={css`
+                  color: #79deff;
+                `}
+              >
+                10,404,174
+                {/* {totalVisit} */}
+              </span>
+              명의 회원이 플로우빗과 함께하고 있습니다.
+              <br />
+              지금 바로 시작하세요!
+            </p>
+            <img
+              src={BlackEtherImg}
+              css={css`
+                position: absolute;
+                bottom: 2px;
+                left: 16%;
+                
+                ${BREAK_POINTS.TABLET} {
+                  width: 7rem;
+                }
+
+                ${BREAK_POINTS.MOBILE} {
+                  width: 3rem;
+                }
+              `}
+            />
+            <img
+              src={SkyblueLImg}
+              css={css`
+                position: absolute;
+                top: 20%;
+                left: 6%;
+
+                ${BREAK_POINTS.TABLET} {
+                  left: 3%;
+                  width: 6rem;
+                }
+
+                ${BREAK_POINTS.MOBILE} {
+                  left: 2px;
+                  top: 5%;
+                  width: 3rem;
+                }
+              `}
+            />
+            <img
+              src={GreenBitImg}
+              css={css`
+                position: absolute;
+                top: 0px;
+                left: 27%;
+
+                ${BREAK_POINTS.TABLET} {
+                  width: 4rem;
+                }
+
+                ${BREAK_POINTS.MOBILE} {
+                  width: 3rem;
+                }
+              `}
+            />
+            <img
+              src={YellowBitImg}
+              css={css`
+                position: absolute;
+                top: 0px;
+                left: 78%;
+
+                ${BREAK_POINTS.TABLET} {
+                  width: 8rem;
+                }
+
+                ${BREAK_POINTS.MOBILE} {
+                  width: 5rem;
+                }
+              `}
+            />
+            <img
+              src={OrangeBitImg}
+              css={css`
+                position: absolute;
+                bottom: 0px;
+                left: 82%;
+
+                ${BREAK_POINTS.TABLET} {
+                  width: 8rem;
+                }
+
+                ${BREAK_POINTS.MOBILE} {
+                  width: 5rem;
+                }
+              `}
+            />
+          </div>
+        </div>
       </section>
     </article>
   );
