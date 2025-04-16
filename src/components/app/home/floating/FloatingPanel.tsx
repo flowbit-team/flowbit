a18e8dfbe1d9f9f2a3f89b89ee5ef4452d17383d
Cllaude99 <xodbsrla1025@naver.com>
2025-04-16 20:47:59 +0900

    feat: 플로팅 패널 컴포넌트 구현 및 탭 기능 추가

---
+++ src/components/app/home/floating/FloatingPanel.tsx
@@ -0,0 +1,62 @@
+import IconAIChartAnalyzeInactive from "@/assets/IconAIChartAnalyzeInactive.svg?react";
+import IconAIChartAnalyzeActive from "@/assets/IconAIChartAnalyzeActive.svg?react";
+import IconAIChartRecommendInactive from "@/assets/IconAIChartRecommendInactive.svg?react";
+import IconAIChartRecommendActive from "@/assets/IconAIChartRecommendActive.svg?react";
+import IconFunctionHelpInactive from "@/assets/IconFunctionHelpInactive.svg?react";
+import IconFunctionHelpActive from "@/assets/IconFunctionHelpActive.svg?react";
+import FloatingPanelLayout from "./FloatingPanelLayout";
+import { useTab } from "@/hooks/useTab";
+import AIChartAnalyze from "./AIChartAnalyze";
+import AIChartRecommendation from "./AIChartRecommendation";
+import FunctionHelp from "./FunctionHelp";
+
+export default function FloatingPanel() {
+  const TabKey = {
+    CHART_ANALYZE: "CHART_ANALYZE",
+    CHART_RECOMMENDATION: "CHART_RECOMMENDATION",
+    FUNCTION_HELP: "FUNCTION_HELP",
+  } as const;
+
+  const { Tabs } = useTab<keyof typeof TabKey>(TabKey.CHART_ANALYZE);
+
+  return (
+    <FloatingPanelLayout>
+      <Tabs direction="column">
+        <Tabs.List direction="row">
+          <Tabs.Trigger
+            value={TabKey.CHART_ANALYZE}
+            activeIcon={<IconAIChartAnalyzeActive />}
+            inactiveIcon={<IconAIChartAnalyzeInactive />}
+          >
+            AI 차트해석
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            value={TabKey.CHART_RECOMMENDATION}
+            activeIcon={<IconAIChartRecommendActive />}
+            inactiveIcon={<IconAIChartRecommendInactive />}
+          >
+            AI 추천판단
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            value={TabKey.FUNCTION_HELP}
+            activeIcon={<IconFunctionHelpActive />}
+            inactiveIcon={<IconFunctionHelpInactive />}
+          >
+            기능 도움말
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.ContentWrapper>
+          <Tabs.Content value={TabKey.CHART_ANALYZE}>
+            <AIChartAnalyze />
+          </Tabs.Content>
+          <Tabs.Content value={TabKey.CHART_RECOMMENDATION}>
+            <AIChartRecommendation />
+          </Tabs.Content>
+          <Tabs.Content value={TabKey.FUNCTION_HELP}>
+            <FunctionHelp />
+          </Tabs.Content>
+        </Tabs.ContentWrapper>
+      </Tabs>
+    </FloatingPanelLayout>
+  );
+}
