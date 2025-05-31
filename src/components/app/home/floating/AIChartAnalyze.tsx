22c3ec7fc1f4963ba6fda8924922751314fad5bf
Cllaude99 <xodbsrla1025@naver.com>
2025-05-31 10:42:37 +0900

    feat: AI 차트 해석 api 연동

--- src/components/app/home/floating/AIChartAnalyze.tsx
+++ src/components/app/home/floating/AIChartAnalyze.tsx
@@ -1,3 +1,81 @@
+import IconRightChevron from "@/assets/IconRightChevron.svg?react";
+import { useBitCoinAnalysisQuery } from "@/hooks/api/floating/useBitCoinAnalysisQuery";
+import { css } from "@emotion/react";
+
 export default function AIChartAnalyze() {
-  return <div>AIChartAnalyze</div>;
+  const { data } = useBitCoinAnalysisQuery();
+  console.log(data);
+
+  return (
+    <div>
+      <h1
+        css={css`
+          font-size: 18px;
+          font-weight: 600;
+          line-height: 1.4;
+        `}
+      >
+        '비트코인' 가격 추세 예측과 뉴스 동향을 종합해서 최적의 투자 판단을
+        추천해드릴게요.
+      </h1>
+      <div
+        css={css`
+          background-color: var(--blue-5);
+          padding: 16px;
+          border-radius: 8px;
+          margin-bottom: 24px;
+        `}
+      >
+        <div
+          css={css`
+            font-weight: 600;
+            font-size: 16px;
+            margin-bottom: 8px;
+          `}
+        >
+          비트코인 예측 해석
+        </div>
+        <div
+          css={css`
+            font-size: 14px;
+            line-height: 1.5;
+            color: var(--gray-80);
+          `}
+        >
+          {data?.response}
+        </div>
+      </div>
+
+      <div>
+        <h2
+          css={css`
+            font-size: 18px;
+            font-weight: 600;
+            line-height: 1.4;
+          `}
+        >
+          지금 주요뉴스 동향을 분석해서 추천할 만한 투자 판단을 도와드릴까요?
+        </h2>
+        <div
+          css={css`
+            width: 100%;
+            height: 40px;
+            margin-top: 12px;
+            background-color: #0056ca;
+            color: white;
+            border-radius: 6px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            font-size: 16px;
+            font-weight: 400;
+            margin: 8.5px 0px;
+          `}
+        >
+          AI 추천판단 <IconRightChevron />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/hooks/api/floating/useBitCoinAnalysisQuery.ts
@@ -0,0 +1,20 @@
+import { api } from "@/api";
+import { useQuery } from "@tanstack/react-query";
+
+export const useBitCoinAnalysisQuery = () => {
+  const getBitCoinAnalysis = async () => {
+    const response = await api.get(
+      "/bitcoin-service/get_chart_analysis?currency=BTC",
+    );
+    return response.data;
+  };
+
+  return {
+    ...useQuery({
+      queryKey: ["bitCoinAnalysis"],
+      queryFn: () => getBitCoinAnalysis(),
+      staleTime: 60000 * 60 * 2, // 2시간
+      gcTime: 60000 * 60 * 2, // 2시간
+    }),
+  };
+};
